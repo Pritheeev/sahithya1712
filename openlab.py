bba84688d0b30fe37f00740f412fad0513edda12
sahithyasenthil <49310690+sahithyasenthil@users.noreply.github.com>
2020-03-13 09:15:06 -0400

    Delete openlab.py

--- openlab.py
+++
@@ -1,5 +0,0 @@
-import urllib.request
-with urllib.request.urlopen('http://python.org/') as response:
-   html = response.read()
-local_filename, headers = urllib.request.urlretrieve('http://python.org/')
-html = open(local_filename)
